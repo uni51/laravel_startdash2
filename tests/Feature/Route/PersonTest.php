0cdc59d4fc47f3a4ea3be1b2cdb0fc2b465b48d7
uni51 <uni51@users.noreply.github.com>
2019-09-25 12:53:08 +0900

    tests/Feature/Route/PersonTest URL check

--- tests/Feature/Route/PersonTest.php
+++ tests/Feature/Route/PersonTest.php
@@ -8,15 +8,34 @@ use Tests\TestCase;
 
 class PersonTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    public function testPersonList()
     {
-        $response = $this->get('/');
-
+        $response = $this->json('GET','api/person');
         $response->assertStatus(200);
     }
+    
+    public function testPersonStore()
+    {
+        $response = $this->json('POST','api/person',
+            ['name'=>'Johnny', 'weight'=>60, 'height'=>'1.72']);
+        $response->assertStatus(200);
+    }
+    
+    public function testPersonView()
+    {
+        $response = $this->json('GET','api/person/1');
+        $response->assertStatus(200);
+    }
+    
+    public function testPersonUpdate()
+    {
+        $response = $this->json('PUT','api/person/1', ['name' => 'Johnson']);
+        $response->assertStatus(200);
+    }
+    
+    public function testPersonDelete()
+    {
+        $response = $this->json('DELETE','api/person/1');
+        $response->assertStatus(200);
+    }    
 }
