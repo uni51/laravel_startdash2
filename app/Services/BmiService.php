98371a22d08bd01a6547add1b2618d0ec82abb2a
uni51 <uni51@users.noreply.github.com>
2019-09-25 13:44:13 +0900

    Create app/Services/BmiService

---
+++ app/Services/BmiService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Person;
+
+class BmiService
+{
+    /*
+    * calc BMI from Person
+    *
+    * @param App\Models\Person $person
+    * @return float or void
+    */
+    public static function getBmi(Person $person)
+    {
+
+    }
+
+    /*
+    * calc BMI
+    *
+    * @param float $height
+    * @param float $height
+    * @return float or void
+    */
+    private static function calcBmi(float $height, float $weight)
+    {
+
+    }
+}
