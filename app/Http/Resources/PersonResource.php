80f2ca3842b74ee5e1d8cc15561494a63c7ffced
uni51 <uni51@users.noreply.github.com>
2019-09-25 14:20:04 +0900

    Update PersonResource

--- app/Http/Resources/PersonResource.php
+++ app/Http/Resources/PersonResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Services\BmiService;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class PersonResource extends JsonResource
@@ -14,6 +15,14 @@ class PersonResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'height' => $this->height,
+            'weight' => $this->weight,
+            'BMI' => BmiService::getBmi($this->resource),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
