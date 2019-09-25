043bad64bf92914b354fd137bebf21731cc06c50
uni51 <uni51@users.noreply.github.com>
2019-09-25 13:45:28 +0900

    php artisan make:test --unit Services/BmiServiceTest

---
+++ tests/Unit/Services/BmiServiceTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BmiServiceTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
